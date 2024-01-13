a9fdeb10618d40fde3113dcbe7d8c694ddc3bf7f
ayushp2563 <ayushprajapati2563@gmail.com>
2024-01-13 14:13:06 +0530

    Update padding and gap in App.css and fix typo in HeroSection.jsx

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
             Developer
           </h1>
           <p className="hero--section-description">
-            I design and build websites that look good, and work well.
+            I design and build websites that look good and work well.
           </p>
         </div>
         <a
