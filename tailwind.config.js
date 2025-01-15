b2f03a6b663885a483b073a9f7724a8b9cdd1fc8
AYUSH PRAJAPATI <ayushprajapati2563@gmail.com>
2025-01-14 22:26:49 -0500

    minor change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   darkMode: "class",
+//   theme: {
+//     extend: {},
+//     colors: {
+//       transparent: "transparent",
+//       current: "currentColor",
+//       primary: "#ffb703",
+//       github: "#023047",
+//       darkblue: "#1c1e53",
+//       black: "#000000",
+//       white: "#ffffff",
+//     },
+//   },
+//   plugins: [require("tailwindcss-elevation"), require("tailwindcss-intersect")],
+// };
 /** @type {import('tailwindcss').Config} */
+import tailwindcssElevation from 'tailwindcss-elevation';
+import tailwindcssIntersect from 'tailwindcss-intersect';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
@@ -14,5 +35,5 @@ export default {
       white: "#ffffff",
     },
   },
-  plugins: [require("tailwindcss-elevation"), require("tailwindcss-intersect")],
+  plugins: [tailwindcssElevation, tailwindcssIntersect],
 };
